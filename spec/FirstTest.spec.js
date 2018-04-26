b65f72b2411ed2abe7befacb8171b5e194db11dd
Elizabeth Wells <elizabeth.a.wells1@gmail.com>
2018-04-26 12:39:33 -0500

    Refactor to include beforeEach

--- spec/FirstTest.spec.js
+++ spec/FirstTest.spec.js
@@ -1,24 +1,24 @@
 var SensorGrid = require('../app/main.js');
 
 describe('sensor grid', () => {
+    let sensorGrid;
+    beforeEach(() => {
+        sensorGrid = new SensorGrid();
+    });
     it('should have a length of 16', ()  => {
-        var sensorGrid = new SensorGrid();
         expect(sensorGrid.grid.length).toBe(16);
     });
 
     it('should have a width of 8', () => {
-        var sensorGrid = new SensorGrid();
         expect(sensorGrid.grid[0].length).toBe(8);
     });
 
     describe('set part on grid', () => {
         it('should set given coordinates on grid to true', () =>{
-            var sensorGrid = new SensorGrid();
             sensorGrid.setPartOnGrid(0,0);
             expect(sensorGrid.grid[0][0]).toBeTruthy();
         });
         it('should set another given coordinates on grid to true', () =>{
-            var sensorGrid = new SensorGrid();
             sensorGrid.setPartOnGrid(0,1);
             expect(sensorGrid.grid[0][1]).toBeTruthy();
         });
@@ -26,12 +26,12 @@ describe('sensor grid', () => {
     describe('get part on grid', () => {
         it('should return true when there is a part on given coordinate', () => {
             // arrange
-            var sensorGrid = new SensorGrid();
             sensorGrid.setPartOnGrid(0,0);
             //act
             const actual = sensorGrid.isPartOnGrid(0,0);
             //asert
             expect(actual).toBeTruthy();
         });
+
     })
 })
